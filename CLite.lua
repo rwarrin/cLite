5736be4731870d81eb9833d4fae1e2c6bf4e2df9
Rick Warrington <phial1020@gmail.com>
2009-09-28 15:44:36 -0600

    Added in the ability to search through a table of zones with a true/false value to decided whether or not to enable combat logging.  Working as planned.
    Signed-off-by: Rick Warrington <phial1020@gmail.com>

--- CLite.lua
+++ CLite.lua
@@ -2,18 +2,39 @@ local zones =
 {
 	["Trial of the Grand Crusader"] = true,
 	["Trial of the Crusader"] = true,
-	["Trial of the Champion"] = false,
-	["Stormwind Stockade"] = true
+	["Trial of the Champion"] = true,
+	["Stormwind Stockade"] = true,
+	["Ironforge"] = true
 }
 
 --This function checks the current zone agaist the table of zones and decides whether or not to enable or disable
 --combat logging.
 local function zoneChangedUpdate()
 	local CurrentZoneText = GetRealZoneText();
+	local LoggingStatus = false;
+	
 	DEFAULT_CHAT_FRAME:AddMessage("CLite zone changed to " .. CurrentZoneText ..".");
 	
+	for key,value in pairs(zones) do
+		if key == CurrentZoneText and value == true then
+			DEFAULT_CHAT_FRAME:AddMessage("Zone found!");
+			LoggingStatus = true;
+			--enable logging
+			break;
+		else
+			DEFAULT_CHAT_FRAME:AddMessage("Zone not found.");
+			LoggingStatus = false;
+			--disable logging
+		end
+	end
 	
-	
+	if(LoggingStatus == true) then
+		DEFAULT_CHAT_FRAME:AddMessage("Logging Started");
+		LoggingCombat(1);
+	else --LoggingStatus == false then
+		DEFAULT_CHAT_FRAME:AddMessage("Logging Stopped");
+		LoggingCombat();
+	end
 	
 end
 
@@ -24,10 +45,7 @@ local function onEvent(self, event)
 	end
 end
 
-
-
 -- Create The Frame and Register Events
-
 local CLite = CreateFrame("FRAME", "CLite_Frame", UIParent);
 DEFAULT_CHAT_FRAME:AddMessage("CLite Loaded");
 
