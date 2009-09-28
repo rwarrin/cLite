2c728b20723521e065f69e02abff99fc54b034cd
Rick Warrington <phial1020@gmail.com>
2009-09-28 17:23:18 -0600

    Fixed Stopped Logging message to only display if Combat was being logged before switching zones.  Also added to ability to choose whether or not to log combat in 10 or 5 man zones.
    Signed-off-by: Rick Warrington <phial1020@gmail.com>

--- CLite.lua
+++ CLite.lua
@@ -1,39 +1,61 @@
+--[[
+	To enable or disable logging in certain zones place either a true or false next to the zone.  If you want to add a new zone
+	to the list just write it exactly like the others already there and make sure you spell it like it's called in the game.
+]]
 local zones = 
 {
 	["Trial of the Grand Crusader"] = true,
 	["Trial of the Crusader"] = true,
 	["Trial of the Champion"] = true,
-	["Stormwind Stockade"] = true,
-	["Ironforge"] = true
+	["Ulduar"] = true,
+	["Obsidian Sanctum"] = true,
+	["Onyxia's Lair"] = true,
+	["Onyxia's Lair"] = true,
+	["Onyxia's Lair"] = true,
+	["Onyxia's Lair"] = true,
+	["Onyxia's Lair"] = true,
+	["The Stockade"] = true
 }
+--Set this to true if you want to log in 10 man instances, other wise set it to false if you only want to log 25 man instances.
+local EnableTenManLogging = false;
+
 
 --This function checks the current zone agaist the table of zones and decides whether or not to enable or disable
 --combat logging.
-local function zoneChangedUpdate()
-	local CurrentZoneText = GetRealZoneText();
-	local LoggingStatus = false;
+local LoggingStatus = false;
+local DisplayStopMessage = true;
 	
-	DEFAULT_CHAT_FRAME:AddMessage("CLite zone changed to " .. CurrentZoneText ..".");
+local function zoneChangedUpdate()
+	local RaidSize = GetNumRaidMembers();
+	
+	if( EnableTenManLogging == false and RaidSize <= 15) then
+		return;
+	end
+	
+		
+	local CurrentZoneText = GetRealZoneText();
 	
 	for key,value in pairs(zones) do
 		if key == CurrentZoneText and value == true then
-			DEFAULT_CHAT_FRAME:AddMessage("Zone found!");
 			LoggingStatus = true;
-			--enable logging
 			break;
 		else
-			DEFAULT_CHAT_FRAME:AddMessage("Zone not found.");
 			LoggingStatus = false;
-			--disable logging
 		end
 	end
 	
 	if(LoggingStatus == true) then
-		DEFAULT_CHAT_FRAME:AddMessage("Logging Started");
+		DEFAULT_CHAT_FRAME:AddMessage("CLite - Combat Logging Started", 1.0, 0.0, 0.0);
 		LoggingCombat(1);
+		DisplayStopMessage = true;
 	else --LoggingStatus == false then
-		DEFAULT_CHAT_FRAME:AddMessage("Logging Stopped");
-		LoggingCombat();
+		if(DisplayStopMessage == true) then
+			DEFAULT_CHAT_FRAME:AddMessage("CLite - Combat Logging Stopped", 1.0, 0.0, 0.0);
+			LoggingCombat();
+			DisplayStopMessage = false;
+		else
+			LoggingCombat();
+		end
 	end
 	
 end
@@ -51,3 +73,5 @@ DEFAULT_CHAT_FRAME:AddMessage("CLite Loaded");
 
 CLite:SetScript("OnEvent", onEvent);
 CLite:RegisterEvent("ZONE_CHANGED_NEW_AREA", "zoneChangedUpdate");
+LoggingCombat();
+zoneChangedUpdate();
