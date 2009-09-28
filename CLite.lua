e0efe6905f49104cf06c8402478d048858c72609
Rick Warrington <phial1020@gmail.com>
2009-09-28 15:15:22 -0600

    Working AddOn now handles ZONE_CHANGED_NEW_AREA event.
    Signed-off-by: Rick Warrington <phial1020@gmail.com>

--- CLite.lua
+++ CLite.lua
@@ -3,17 +3,33 @@ local zones =
 	["Trial of the Grand Crusader"] = true,
 	["Trial of the Crusader"] = true,
 	["Trial of the Champion"] = false,
-	["Stockades"] = true
+	["Stormwind Stockade"] = true
 }
 
-local frame CLite = CreateFrame("FRAME");
-
-function CLite:OnEnable()
-{
-	CLite:RegisterEvent("ZONE_CHANGED_NEW_AREA", zoneChanged());
-}
-
-function zoneChanged()
-{
+--This function checks the current zone agaist the table of zones and decides whether or not to enable or disable
+--combat logging.
+local function zoneChangedUpdate()
+	local CurrentZoneText = GetRealZoneText();
+	DEFAULT_CHAT_FRAME:AddMessage("CLite zone changed to " .. CurrentZoneText ..".");
 	
-}
+	
+	
+	
+end
+
+--OnEvent script handler.
+local function onEvent(self, event)
+	if(event == "ZONE_CHANGED_NEW_AREA")then
+		zoneChangedUpdate();
+	end
+end
+
+
+
+-- Create The Frame and Register Events
+
+local CLite = CreateFrame("FRAME", "CLite_Frame", UIParent);
+DEFAULT_CHAT_FRAME:AddMessage("CLite Loaded");
+
+CLite:SetScript("OnEvent", onEvent);
+CLite:RegisterEvent("ZONE_CHANGED_NEW_AREA", "zoneChangedUpdate");
