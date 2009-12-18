25d1d3c5c10fc9a10f9d3f32a1fa2aa653f39c99
Rick Warrington <phial1020@gmail.com>
2009-12-18 01:38:57 -0700

    Updated list of instances to include the new 10 and 25 man raids, as well as the three new 5 man instances.
    Signed-off-by: Rick Warrington <phial1020@gmail.com>

--- CLite.lua
+++ CLite.lua
@@ -15,8 +15,10 @@ local zones =
 	["The Eye of Eternity"] = true,
 	["Vault of Archavon"] = true,
 	["Onyxia's Lair"] = true,
+	["Icecrown Citadel"] = true,
 	
 	--Heroic Instances
+	["Trial of the Champion"] = false,
 	["The Old Kingdom"] = false,
 	["Drak'Tharon Keep"] = false,
 	["Gundrak"] = false,
@@ -27,14 +29,14 @@ local zones =
 	["Halls of Stone"] = false,
 	["Utgarde Keep"] = false,
 	["Utgarde Pinnacle"] = false,
-	["Trial of the Champion"] = false,
+	["The Forge of Souls"] = false,
+	["Pit of Saron"] = false,
+	["Halls of Reflection"] = false,
 }
 --Set this to true if you want to log in 10 man instances too, other wise set it to false if you only want to log 25 man instances.
 local EnableTenManLogging = true;
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------CONFIG---------------------------------------------------------------------------------------
-
-
 
 --This function checks the current zone agaist the table of zones and decides whether or not to enable or disable
 --combat logging.
@@ -44,14 +46,14 @@ local DisplayStopMessage = true;
 local function zoneChangedUpdate()
 	local RaidSize = GetNumRaidMembers();
 	
-	if( EnableTenManLogging == false and RaidSize <= 15) then
+	if (EnableTenManLogging == false and RaidSize <= 15) then
 		return;
 	end
 	
 	local CurrentZoneText = GetRealZoneText();
 	
 	for key,value in pairs(zones) do
-		if key == CurrentZoneText and value == true then
+		if (key == CurrentZoneText and value == true) then
 			LoggingStatus = true;
 			break;
 		else
@@ -63,8 +65,8 @@ local function zoneChangedUpdate()
 		DEFAULT_CHAT_FRAME:AddMessage("CLite - Combat Logging Started", 1.0, 0.0, 0.0);
 		LoggingCombat(1);
 		DisplayStopMessage = true;
-	else --LoggingStatus == false then
-		if(DisplayStopMessage == true) then
+	else --if (LoggingStatus == false) then
+		if (DisplayStopMessage == true) then
 			DEFAULT_CHAT_FRAME:AddMessage("CLite - Combat Logging Stopped", 1.0, 0.0, 0.0);
 			LoggingCombat(0);
 			DisplayStopMessage = false;
@@ -76,7 +78,7 @@ end
 
 --OnEvent script handler.
 local function onEvent(self, event)
-	if(event == "ZONE_CHANGED_NEW_AREA")then
+	if (event == "ZONE_CHANGED_NEW_AREA") then
 		zoneChangedUpdate();
 	end
 end
