cce7408a0bcd185e21fa17a780e79d35e863f822
unknown <Rick@.(none)>
2010-02-27 17:12:02 -0700

    Removed some un-needed stuff
    Signed-off-by: unknown <Rick@.(none)>

--- CLite.lua
+++ CLite.lua
@@ -1,5 +1,4 @@
 -----------------------------------------------------------------------------------CONFIG---------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --[[
 	To enable or disable logging in certain zones place either a true or false next to the zone.  If you want to add a new zone
 	to the list just write it exactly like the others already there and make sure you spell it like it's called in the game.
@@ -17,9 +16,8 @@ local zones =
 	["Onyxia's Lair"] = true,
 	["Icecrown Citadel"] = true,
 }
---Set this to true if you want to log in 10 man instances too, other wise set it to false if you only want to log 25 man instances.
+--Set this to true if you want to log in 10 man instances too, otherwise set it to false if you only want to log 25 man instances.
 local EnableTenManLogging = true;
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------CONFIG---------------------------------------------------------------------------------------
 
 --This function checks the current zone agaist the table of zones and decides whether or not to enable or disable
