38e438bfa065f0c544d7bb1ea45c195416ace260
Rick Warrington <phial1020@gmail.com>
2012-04-19 06:34:29 -0600

    Create frame, register events, slash command added
    Created a frame for the AddOn and registered for events and set a handler
    for the events.  Added a slash command to toggle automatic logging.

--- core.lua
+++ core.lua
@@ -16,4 +16,13 @@ local zones =
 
 -------------------------------------------------------------------------------
 
+-- Create AddOn frame and register for events
+local cLite = CreateFrame("FRAME", "cLiteFrame", UIParent);
+cLite:RegisterEvent("ADDON_LOADED");
+cLite:RegisterEvent("PLAYER_LOGIN");
+cLite:RegisterEvent("ZONE_CHANGED_NEW_AREA");
+cLite:SetScript("OnEvent", OnEvent);
 
+-- Slash command for toggling automatic logging
+SLASH_CLITETOGGLE1 = "/cl";
+SlashCmdList["CLITETOGGLE"] = ToggleAutomaticLogging;
