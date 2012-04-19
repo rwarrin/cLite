4cc8b86c30ccd25a6c4f6e04bec0709700a92c7a
Rick Warrington <phial1020@gmail.com>
2012-04-19 17:08:11 -0600

    Changed how events are handled in OnEvent.
    Merged ZONE_CHANGED_NEW_AREA and PLAYER_LOGIN events into the same if
    statement since they call the same function.

--- core.lua
+++ core.lua
@@ -45,14 +45,12 @@ end
 
 -- Handle events
 local function OnEvent(self, event, ...)
-    if(event == "ZONE_CHANGED_NEW_AREA") then
+    if(event == "ZONE_CHANGED_NEW_AREA" or "PLAYER_LOGIN") then
         UpdateLoggingStatus();
     elseif(event == "ADDON_LOADED") then
         if(enabled == nil) then
             enabled = true;
         end
-    elseif(event == "PLAYER_LOGIN") then
-        UpdateLoggingStatus();
     end
 end
 
